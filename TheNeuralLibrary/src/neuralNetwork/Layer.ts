cfb278e3b6fb5ad48be9f136bea4c6b5a83145b1
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-06-17 17:54:09 -0500

    layer interface done

---
+++ TheNeuralLibrary/src/neuralNetwork/Layer.ts
@@ -0,0 +1,11 @@
+
+interface Layer{
+    input: number[];
+    output: number[];
+
+    forwardPropagation(inputData:number[]):number[];
+
+    backPropagation(errorRespectToOutput: number[], learningRate: number):number[];
+}
+
+export{Layer};
