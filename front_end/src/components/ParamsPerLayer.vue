e43f8fcd53906af42cc6e8824b122ffdfb08ceba
Brik000 <35845838+Brik000@users.noreply.github.com>
2020-09-07 14:46:52 -0500

    updste

--- front_end/src/components/ParamsPerLayer.vue
+++ front_end/src/components/ParamsPerLayer.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    
-    <label for="actfunction">Activation Function: </label>
+  <div class="row">
+    <label for="actfunction">Activation Function:</label>
     <dropdown
       :options="allActFunctions"
       :selected="layer.actFunction"
@@ -9,7 +8,7 @@
       :placeholder="'Select an Item'"
       :closeOnOutsideClick="true"
     >></dropdown>
-    <label for="lossFunc">Ammount of neurons: </label>
+    <label for="lossFunc">Ammount of neurons:</label>
     <dropdown
       :options="neuronsArray"
       :selected="layer.neurons"
@@ -18,21 +17,59 @@
       :closeOnOutsideClick="true"
     >></dropdown>
 
-   
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-4">
+          <h4 class="card-title">Select ammount of neurons</h4>
+          <el-select
+            class="select-primary"
+            size="large"
+            placeholder="Amount of neurons"
+            v-model="layer.neurons"
+          >
+            <el-option
+              v-for="option in neuronsArray"
+              class="select-primary"
+              :value="option"
+              :label="option"
+              :key="option"
+            ></el-option>
+          </el-select>
+        </div>
+
+        <div class="col-md-6">
+          <h4 class="card-title">Activation Function</h4>
+          <el-select
+            class="select-primary"
+            size="large"
+            placeholder="Select an actFunc"
+            v-model="layer.actFunction"
+          >
+            <el-option
+              v-for="option in allActFunctions"
+              class="select-primary"
+              :value="option"
+              :label="option"
+              :key="option"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script >
 import dropdown from "vue-dropdowns";
-import{Network} from "../../TheNeuralLibrary/src/neuralNetwork/Network.js";
+import { Network } from "../../TheNeuralLibrary/src/neuralNetwork/Network.js";
 
+import { Select, Option } from "element-ui";
 
 export default {
-   name: 'ParamsPerLayer',
-   
+  name: "ParamsPerLayer",
+
   data() {
-    
     return {
       layer: {
         actFunction: "",
@@ -40,65 +77,50 @@ export default {
       },
       // debe llegar del back
       allActFunctions: [],
-       net:Network,
-      
+      net: Network,
 
       // esos valores toca pedirlos del backend
-     
-      neuronsArray:null,
-      
+
+      neuronsArray: null,
     };
   },
 
   components: {
     dropdown: dropdown,
+    [Option.name]: Option,
+    [Select.name]: Select,
   },
   created() {
-  this.net=new Network();
+    this.net = new Network();
 
     //pedir los maximos de cada vaina
     this.setLimimtArrays();
-    this.allActFunctions =this.net
-      .getAllActivationFunctions();
-
+    this.allActFunctions = this.net.getAllActivationFunctions();
   },
 
   methods: {
     setLimimtArrays() {
-      
-     this.neuronsArray=Array.from(Array(this.net.getMaxNumberOfNeurons()), (_, i) => i + 1);
-
-
-
-      },
-      methodToRunOnSelectActivation(payload){
-         this.layer.actFunction = payload;
-      },
-    
+      this.neuronsArray = Array.from(
+        Array(this.net.getMaxNumberOfNeurons()),
+        (_, i) => i + 1
+      );
+    },
+    methodToRunOnSelectActivation(payload) {
+      this.layer.actFunction = payload;
+    },
 
     methodToRunOnSelectNumber(payload) {
       alert(payload);
       this.layers.neurons = payload;
     },
-    },
-  
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.row .el-select {
+  width: 100%;
+  margin-bottom: 30px;
 }
 </style>
