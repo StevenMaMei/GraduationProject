06afe68608adca4521777bf497633bcd198868b6
Jose Alejandro Galvis Nieto <jose.galvis@correo.icesi.edu.co>
2020-09-23 11:30:52 -0500

    working on d3 visualization

--- front_end/src/views/Network.vue
+++ front_end/src/views/Network.vue
@@ -8,7 +8,7 @@
 import * as d3 from 'd3'
 import jsonData from '../assets/json/simple_force_layout.json'
 export default {
-  name: 'ForceBasedLabelPlacement',
+  name: 'Network',
   components: {
   },
   mounted () {
@@ -29,23 +29,31 @@ export default {
       })
     })
     let labelLayout = d3.forceSimulation(label.nodes)
-      .force('charge', d3.forceManyBody().strength(-50))
+      .force('charge', d3.forceManyBody().strength(-10))
       .force('link', d3.forceLink(label.links).distance(0).strength(2))
+
+    
+    
     d3.forceSimulation(graph.nodes)
-      .force('charge', d3.forceManyBody().strength(-3000))
+      /* .force('charge', d3.forceManyBody().strength(-3000)) */
       .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('x', d3.forceX(width / 2).strength(1))
-      .force('y', d3.forceY(height / 2).strength(1))
-      .force('link', d3.forceLink(graph.links).id(function (d) { return d.id }).distance(50).strength(1))
+      .force('x', d3.forceX(function(d){
+        return d.layer*400;
+      }).strength(1))
+      .force('y', d3.forceY(function(){
+        return 100  + Math.floor(Math.random()*200);
+      }).strength(1))
+      .force('link', d3.forceLink(graph.links).id(function (d) { return d.id }).distance(0).strength(1))
       .on('tick', ticked)
-    let adjlist = []
+
+
+
+    /* let adjlist = []
     graph.links.forEach(function (d) {
       adjlist[d.source.index + '-' + d.target.index] = true
       adjlist[d.target.index + '-' + d.source.index] = true
-    })
-    // function neigh (a, b) {
-    //   return a === b || adjlist[a + '-' + b]
-    // }
+    }) */
+    
     let svg = d3.select('#viz').attr('width', width).attr('height', height)
     let container = svg.append('g')
     svg.call(
@@ -69,13 +77,8 @@ export default {
       .append('circle')
       .attr('r', 5)
       .attr('fill', function (d) { return color(d.group) })
-    // node.on('mouseover', focus).on('mouseout', unfocus)
-    // node.call(
-    //   d3.drag()
-    //     .on('start', dragstarted)
-    //     .on('drag', dragged)
-    //     .on('end', dragended)
-    // )
+
+
     let labelNode = container.append('g').attr('class', 'labelNodes')
       .selectAll('text')
       .data(label.nodes)
@@ -86,7 +89,8 @@ export default {
       .style('font-family', 'Arial')
       .style('font-size', 12)
       .style('pointer-events', 'none') // to prevent mouseover/drag capture
-    // node.on('mouseover', focus).on('mouseout', unfocus)
+    
+
     function ticked () {
       node.call(updateNode)
       link.call(updateLink)
@@ -112,23 +116,8 @@ export default {
       if (isFinite(x)) return x
       return 0
     }
-    // function focus () {
-    //   let index = d3.select(d3.event.target).datum().index
-    //   node.style('opacity', function (o) {
-    //     return neigh(index, o.index) ? 1 : 0.1
-    //   })
-    //   labelNode.attr('display', function (o) {
-    //     return neigh(index, o.node.index) ? 'block' : 'none'
-    //   })
-    //   link.style('opacity', function (o) {
-    //     return o.source.index === index || o.target.index === index ? 1 : 0.1
-    //   })
-    // }
-    // function unfocus () {
-    //   labelNode.attr('display', 'block')
-    //   node.style('opacity', 1)
-    //   link.style('opacity', 1)
-    // }
+
+
     function updateLink (link) {
       link.attr('x1', function (d) { return fixna(d.source.x) })
         .attr('y1', function (d) { return fixna(d.source.y) })
@@ -137,25 +126,9 @@ export default {
     }
     function updateNode (node) {
       node.attr('transform', function (d) {
-        // console.log(fixna(d.x) + ' , ' + fixna(d.y))
         return 'translate(' + fixna(d.x) + ',' + fixna(d.y) + ')'
       })
     }
-    // function dragstarted (d) {
-    //   d3.event.sourceEvent.stopPropagation()
-    //   if (!d3.event.active) graphLayout.alphaTarget(0.3).restart()
-    //   d.fx = d.x
-    //   d.fy = d.y
-    // }
-    // function dragged (d) {
-    //   d.fx = d3.event.x
-    //   d.fy = d3.event.y
-    // }
-    // function dragended (d) {
-    //   if (!d3.event.active) graphLayout.alphaTarget(0)
-    //   d.fx = null
-    //   d.fy = null
-    // }
   }
 }
 </script>
