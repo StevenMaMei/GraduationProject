82e74e30bb0b8380ade711f140267a5c7cb55383
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-09-23 19:08:40 -0500

    login and register completed

--- back_end/services/AuthenticationService.js
+++ back_end/services/AuthenticationService.js
@@ -25,6 +25,46 @@ class AuthenticationService {
                });
             })
     }
+
+    async login(userCredentials, res){
+        let {email, password} = userCredentials;
+        User.findOne({ email: email }, (erro, userDB)=>{
+            if (erro) {
+                return res.status(500).json({
+                    ok: false,
+                    err: erro
+                })
+            }
+            if (!userDB) {
+                return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Incorrect user or password"
+                }
+                })
+            }
+
+            if (! bcrypt.compareSync(password, userDB.password)){
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                    message: "Incorrect user or password"
+                    }
+                });
+            }
+
+            let token = jwt.sign({
+                    usuario: userDB,
+                }, process.env.AUTHENTICATION_SEED, {
+                expiresIn: process.env.TOKEN_EXPIRATION
+            })
+            res.json({
+                ok: true,
+                usuario: userDB,
+                token,
+            })
+        })
+    }
 }
 
 module.exports = {
