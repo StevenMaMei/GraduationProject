2eaaf5c503a59db308214e2e3e39c8dcb3fa7076
Jose Alejandro Galvis Nieto <jose.galvis@correo.icesi.edu.co>
2020-09-30 11:00:18 -0500

    login and register options added to front end

---
+++ front_end/src/views/MainPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app>
+    <main>
+      <Network/>
+      <hr />
+      <ParamsNetwork />
+    </main>
+  </v-app>
+</template>
+
+<script>
+import Network from "./Network"; 
+import ParamsNetwork from "./ParamsNetwork";
+export default {
+  name: "Main",
+  components: {
+    Network,
+    ParamsNetwork
+  },
+  data() {
+    return {
+    };
+  },
+  created(){
+    
+  }
+  
+};
+</script>
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Roboto|Material+Icons");
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+main {
+  font-family: "Roboto", "sans-serif";
+  background: #fff top center repeat;
+  color: #444;
+}
+h1,
+p {
+  margin: 0 0 1em 0;
+}
+.app-container {
+  max-width: 100%;
+  margin: 0 auto;
+  /* background-color: #e8e8e8; */
+  background-color: #fff;
+}
+.app-container > * {
+  border-radius: 5px;
+  font-size: 150%;
+  margin-bottom: 3%;
+}
+img {
+  max-width: 100%;
+  display: block;
+  margin: 0 auto;
+}
+</style>
---
+++ front_end/src/views/Register.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h2 class="margin">Registration</h2>
+    <form @submit.prevent="createUser">
+
+      <v-text-field class="margin" v-model="firstName" label="First Name"></v-text-field>
+      <v-text-field class="margin" v-model="lastName" label="Last Name"></v-text-field>
+
+
+      <v-text-field class="margin" v-model="email" label="Email"></v-text-field>
+      <v-text-field
+        v-model="password"
+        :type="'password'"
+        label="Password"
+        class="input-group--focused margin"
+      ></v-text-field>
+
+      
+
+      <v-btn type="submit" color="green" class="margin">REGISTER</v-btn>
+      <v-row class="margin"><p class="loginOption" >Already have an account? </p><router-link class="loginOption" to="/login"> Sign in here!</router-link></v-row>
+    </form>
+  </div>
+</template>
+
+<script>
+/* import axios from '../plugins/axios' */
+export default {
+  data() {
+    return {
+      user: {
+        firstName: "",
+        lastName: "",
+        password: "",
+        email: "",
+        edad: 0,
+        
+       
+      }
+     
+    };
+  },
+  methods: {      
+      createUser(){
+          
+      }
+     
+      
+  } 
+};
+</script>
+
+<style>
+.margin {
+  margin-top: 1%;
+}
+.loginOption{
+  text-align: center;
+  font-style: italic;
+  font-size: medium;
+  color: gray;
+}
+
+</style>
